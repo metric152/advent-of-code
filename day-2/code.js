278ea0db4d90611073d53c7f408c183ffb94968c
ernest leitch <ernest.leitch@disney.com>
2015-12-02 11:43:46 -0800

    day 2: part 2

--- day-2/code.js
+++ day-2/code.js
@@ -4,6 +4,7 @@
         var data = document.getElementById("instructions").value.split("\n");
         var presents = [];
         var total = 0;
+        var ribbon = 0;
 
         // Loop through presents
         data.forEach(function(dimensions){
@@ -12,12 +13,13 @@
                 'l': +tmp[0],
                 'w': +tmp[1],
                 'h': +tmp[2],
-                'total': 0
+                'total': 0,
+                'ribbon': 0
             };
-            var smallestSide = 0;
+            var smallest = 0;
 
             // Calculate surface area of each side
-            present.sides = [(present.l * present.w), (present.w * present.h),(present.h * present.l)];
+            present.sides = [(present.l * present.w), (present.w * present.h), (present.h * present.l)];
 
             // Get surface area of the present
             present.sides.map(function(val){
@@ -25,14 +27,33 @@
             });
 
             // Add the smallest side
-            present.total += Math.min.apply(null, present.sides);
+            smallest = Math.min.apply(null, present.sides);
+            present.total += smallest;
+
+            // Figure out the ribbon
+            switch(present.sides.indexOf(smallest)){
+                case 0:
+                    present.ribbon = present.l + present.l + present.w + present.w + (present.l * present.w * present.h);
+                    break;
+                case 1:
+                    present.ribbon = present.w + present.w + present.h + present.h + (present.l * present.w * present.h);
+                    break;
+                case 2:
+                    present.ribbon = present.h + present.h + present.l + present.l + (present.l * present.w * present.h);
+                    break;
+                default:
+                    break;
+            }
 
             // Add present to the array
             presents.push(present);
 
             // Add to overall total
             total += present.total;
+
+            // Add to overall ribbon
+            ribbon += present.ribbon;
         });
-        alert("Paper required:" + total);
+        alert("Paper required:" + total + " ribbon:" + ribbon);
     });
 })();
